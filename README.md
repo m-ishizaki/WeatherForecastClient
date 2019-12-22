47d02d15615cb121f204a3102ac91a9d5ab4db44
m ishizaki <love.ms.is12t@hotmail.co.jp>
2019-12-22 21:41:05 +0900

    Initial Commit.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WeatherForecastClient
+
+.NET Core のテンプレートで生まれた WebAPI プロジェクトのクライアントコードを [AutoRest](https://github.com/Azure/autorest) で生成し、生成されたコードを使ったクライアントアプリを作ってみた .NET Core のコンソールアプリのコードです。  
+
+swagger.json が生成に使った json です。  
+before が生成されたコードです。  
+after が before にコンソールアプリとしてのコードを追加した実際に動かせるプロジェクトコードです。
---
+++ after/CSharp_Swagger/Program.cs
@@ -0,0 +1,19 @@
+﻿using swagger;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CSharp_Swagger
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IMyAPI myAPI = new MyAPI(new Uri("http://localhost:5000/"));
+            var weathers = await myAPI.GetWeatherForecastAsync();
+            var json = JsonSerializer.Serialize(weathers);
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+}
